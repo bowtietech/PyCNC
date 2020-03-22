517a428099624d39170b8eac27abd1d21953c56d
Gregory Magruder <greg@bowtietechnology.com>
2020-03-22 02:09:26 +0000

    Update for Pi Zero W (add revision as Pi O.G.).

--- cnc/hal_raspberry/rpgpio_private.py
+++ cnc/hal_raspberry/rpgpio_private.py
@@ -20,7 +20,7 @@ try:
         RPI_1_REVISIONS = ['0002', '0003', '0004', '0005', '0006', '0007',
                            '0008', '0009', '000d', '000e', '000f', '0010',
                            '0011', '0012', '0013', '0014', '0015', '900021',
-                           '900032']
+                           '900032', '9000c1']
         if h is None:
             raise ImportError("This is not raspberry pi board.")
         elif r.group(1) in RPI_1_REVISIONS:
